d3e9976ba838107f976213448550a1ee3735e71c
PauSmu <75333629+PauSmu@users.noreply.github.com>
2021-02-03 13:52:06 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # repo-158347-
+# To jest projekt Pauliny Smutek <h1> 
+## Na zajęcia Zarządznie Przedsięwzięciami Informatycznymi <h2> 
+###### Wydział Mechaniczno-Technologiczny  <h6> 
---
+++ mysin.cpp
@@ -0,0 +1,34 @@
+#include "mysin.h"
+MySin::MySin()
+{
+	
+}
+
+MySin::(double x)
+{
+	
+}
+MySin::(const MySin &obj)
+{
+	
+}
+MySin::~MySin()
+{
+	
+}
+MySin::double value()
+{
+	return sin(x);
+}
+MySin::void setX(double)
+{
+	
+}
+MySin::double getX()
+{
+	
+}
+private MySin:: double mX()
+{
+	
+}
